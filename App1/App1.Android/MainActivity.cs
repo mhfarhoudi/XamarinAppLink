a10b99c7312e0d1a85c963417187b094ce5f1b8a
Hossein Farhoudi <mh.farhoudi@outlook.com>
2021-03-23 08:32:58 +0430

    Add project files.

---
+++ App1/App1.Android/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+
+namespace App1.Droid
+{
+    [Activity(Label = "App1", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    //Invite App Link
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+                  DataScheme = "https",
+                  DataHost = "app.matrixcloud.ir",
+                  DataPathPrefix = "/Privacy",
+                  AutoVerify = true,
+                  Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable })]
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+                  DataScheme = "http",
+                  DataHost = "app.matrixcloud.ir",
+                  AutoVerify = true,
+                  DataPathPrefix = "/Privacy",
+                  Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable })]
+
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ App1/App1/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        protected override void OnAppLinkRequestReceived(Uri uri)
+        {
+            if (uri.Host.EndsWith("app.matrixcloud.ir", StringComparison.OrdinalIgnoreCase))
+            {
+
+                if (uri.Segments != null && uri.Segments.Length == 2)
+                {
+                    var action = uri.Segments[1].Replace("/", "");
+
+                    switch (action)
+                    {
+                        case "Privacy":
+                                Device.BeginInvokeOnMainThread(async () =>
+                                {
+                                    await Current.MainPage.DisplayAlert("hello", "Welcome Back ", "ok");
+                                });
+                            break;
+
+                        default:
+                            Device.OpenUri(uri);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
